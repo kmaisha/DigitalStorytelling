ef752f5949170c5022ff9a1474f1192a41dfa74a
Tara Langhorne <tara.langhorne@ufl.edu>
2019-11-03 16:37:25 -0500

    Added Transitional Scripts and Scene Framework
    Added some scripts for transitioning between dialogue groups. Also added blank scenes for all story parts

---
+++ DigitalStorytelling/Assets/Scripts/ClickNextDialogueSound.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickNextDialogueSound : MonoBehaviour
+{
+    //public GameObject buttonClicking;
+    public GameObject pastText; // group of text/buttons that you have read - will disappear
+    public GameObject newText; // group of text/buttons that is next - will appear
+    public AudioClip sound; // sound effect to play when transitioning
+
+    void ClickChoiceSound()
+    {
+        newText.SetActive(true); // makes new text/buttons appear
+        pastText.SetActive(false); // makes old text/buttons disappear
+        AudioSource.PlayClipAtPoint(sound, transform.position);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
